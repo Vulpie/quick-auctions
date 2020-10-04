691d2507f6bb2c152e25fcca812926801946acdf
Vulpie <wojtas_f@outlook.com>
2020-10-04 20:33:59 +0200

    feat: add protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,19 @@ import { Switch, Route } from 'react-router-dom'
 import Home from './components/Views/Home'
 import Profile from './components/Views/Profile'
 import Savings from './components/Views/Savings'
+import ProtectedRoute from './auth/ProtectedRoute'
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/profile" exact component={Profile} />
-                <Route path="/savings" exact component={Savings} />
-            </Switch>
-        </div>
-    )
+	return (
+		<div className="App">
+			<Header />
+			<Switch>
+				<Route path="/" exact component={Home} />
+				<ProtectedRoute path="/profile" exact component={Profile} />
+				<ProtectedRoute path="/savings" exact component={Savings} />
+			</Switch>
+		</div>
+	)
 }
 
 export default App
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import { BrowserRouter as Router } from 'react-router-dom'
-import Auth0ProviderWithHistory from './auth0'
+import Auth0ProviderWithHistory from './auth/auth0'
 
 ReactDOM.render(
 	<React.StrictMode>
